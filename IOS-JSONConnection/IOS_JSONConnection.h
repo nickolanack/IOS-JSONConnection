62af7d5abc3322d8e5a5f80e3c2217902b8d8886
nickolanack <nickblackwell82@gmail.com>
2015-05-29 13:38:28 -0700

    Initial Commit

---
+++ IOS-JSONConnection/IOS_JSONConnection.h
@@ -0,0 +1,13 @@
+//
+//  IOS_JSONConnection.h
+//  IOS-JSONConnection
+//
+//  Created by Nick Blackwell on 2015-05-29.
+//  Copyright (c) 2015 Nick Blackwell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IOS_JSONConnection : NSObject
+
+@end
---
+++ IOS-JSONConnection/IOS_JSONConnection.m
@@ -0,0 +1,13 @@
+//
+//  IOS_JSONConnection.m
+//  IOS-JSONConnection
+//
+//  Created by Nick Blackwell on 2015-05-29.
+//  Copyright (c) 2015 Nick Blackwell. All rights reserved.
+//
+
+#import "IOS_JSONConnection.h"
+
+@implementation IOS_JSONConnection
+
+@end
