a0e54592c036c9a74418a38a6cef1608b30c30a9
nickolanack <nickblackwell82@gmail.com>
2015-05-29 14:09:57 -0700

    added test class, (updated scheme - as per travis docs)

---
+++ IOS-JSONConnectionTests/JsonConnectionTest.m
@@ -0,0 +1,40 @@
+//
+//  JsonConnectionTest.m
+//  IOS-JSONConnection
+//
+//  Created by Nick Blackwell on 2015-05-29.
+//  Copyright (c) 2015 Nick Blackwell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface JsonConnectionTest : XCTestCase
+
+@end
+
+@implementation JsonConnectionTest
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
